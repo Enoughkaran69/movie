fae73240280a53e27f33e8e9409476e53271cf2d
ENOUGH KARAN <138587207+Enoughkaran69@users.noreply.github.com>
2024-12-27 17:33:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const cors = require('cors');
-
+const fetch = require('node-fetch');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-// Enable CORS
-app.use(cors());
-
-app.get('/', (req, res) => {
-  res.send('Hello, Node.js app with CORS support!');
+app.get('/proxy', async (req, res) => {
+  const url = 'https://iosmirror.cc/search.php?s=mismatched';
+  
+  try {
+    const response = await fetch(url);
+    const data = await response.text();
+    res.send(data); // Send the data back to the client
+  } catch (error) {
+    res.status(500).send('Error fetching data');
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
