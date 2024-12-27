4098a6b3e0b0d39c59090f2d6ccb544eac887fe6
ENOUGH KARAN <138587207+Enoughkaran69@users.noreply.github.com>
2024-12-27 17:05:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/search', (req, res) => {
     // Your response data
     res.json({ message: "Data from API" });
 });
+app.get('/', (req, res) => {
+  res.send('Hello, Node.js app is deployed on Vercel!');
+});
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
